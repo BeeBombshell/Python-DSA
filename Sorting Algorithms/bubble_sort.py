dfeb1a97c19c589945694104969b96f98dc180d7
Rushil Shah <rushil180101@gmail.com>
2021-10-04 18:13:57 +0530

    Added optimized function for bubble sort.

--- Sorting Algorithms/bubble_sort.py
+++ Sorting Algorithms/bubble_sort.py
@@ -7,3 +7,15 @@ def bubble_sort(array):
                 array[j+1] = temp
     return array
 
+
+def bubble_sort_optimized(array):
+    for i in range(len(array)):
+        swapped = False
+        for j in range(len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                swapped = True
+
+        if not swapped:
+            break
+
