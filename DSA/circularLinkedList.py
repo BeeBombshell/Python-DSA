df92523525341f05261d68bfef8a3f2888e3cb68
aarjav24 <68058926+aarjav24@users.noreply.github.com>
2022-10-01 16:30:29 +0400

    Define function addToEmpty

--- DSA/circularLinkedList.py
+++ DSA/circularLinkedList.py
@@ -6,3 +6,18 @@ class Node:
 class CircularLinkedList:
     def __init__(self):
         self.last = None
+
+    def addToEmpty(self, data):
+
+        if self.last != None:
+            return self.last
+
+        # allocate memory to the new node and add data to the node
+        newNode = Node(data)
+
+        # assign last to newNode
+        self.last = newNode
+
+        # create link to iteself
+        self.last.next = self.last
+        return self.last
