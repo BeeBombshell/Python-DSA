3679490c4771fef9fcc4b7339d0ba4c2b7ed4b96
Shantanu kumar <91122615+ShantanuK86@users.noreply.github.com>
2022-10-06 14:30:14 +0530

    Update lucky_Numbers using recursion.py

--- Recursion/lucky_Numbers using recursion.py
+++ Recursion/lucky_Numbers using recursion.py
@@ -1,6 +1,13 @@
-#User function Template for python3
-
-
+#problem statement
+'''Lucky numbers are subset of integers.let us see the process of arriving at lucky numbers,
+Take the set of integers
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,……
+First, delete every second number, we get following reduced set.
+1, 3, 5, 7, 9, 11, 13, 15, 17, 19,…………
+Now, delete every third number, we get
+1, 3, 7, 9, 13, 15, 19,….….
+Continue this process indefinitely……
+Any number that does NOT get deleted due to above process is called “lucky”.'''
 class Solution:
     def lucky(self,n,i):
         if(n%i==0):
