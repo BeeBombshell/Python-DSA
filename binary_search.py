557575afb428d3ab06f495fc8209984618d079fb
Vaishnavi Shankar Dayal <47249512+vaishnavii15@users.noreply.github.com>
2022-10-08 11:47:19 +0530

    Binary Search #109
    takes input array from user and also the element to be searched

---
+++ binary_search.py
@@ -0,0 +1,25 @@
+def binarySearch(arr, l, r, x):
+    if r >= l:
+        mid = l + (r - l) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binarySearch(arr, l, mid-1, x)
+ 
+        else:
+            return binarySearch(arr, mid + 1, r, x)
+ 
+    else:
+        return -1
+ 
+ 
+arr = [int(i) for i in input().split())
+x = int(input())
+ 
+result = binarySearch(arr, 0, len(arr)-1, x)
+ 
+if result != -1:
+    print("Element is present at index % d" % result)
+else:
+    print("Element is not present in array")
